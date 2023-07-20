7979cfbd2dd101c584f2526c92b699bcf9c1ac0c
gthampi <gt6863@rit.edu>
2023-07-20 14:01:28 -0400

    data models with sqlalchemy

--- app/models.py
+++ app/models.py
@@ -1,16 +1,45 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
 from sqlalchemy.ext.declarative import declarative_base
-import datetime
+from sqlalchemy_utils import generic_repr, Timestamp
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
-class Example(Base):
-    __tablename__ = "business"
+@generic_repr
+class Business(Base, Timestamp):
+    __tablename__ = "businesses"
 
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
-    example_column = Column(String(100), nullable=False)
-    created_at = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.utcnow)
-    updated_at = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.utcnow)
-    created_by = Column(String(100), nullable=True)
-    updated_by = Column(String(100), nullable=True)
+    name = Column(String(100), nullable=False)
+
+    # Define the relationship between Diagnosis and Business
+    diagnosis = relationship("Diagnosis", backref="business")
+
+
+@generic_repr
+class Symptom(Base, Timestamp):
+    __tablename__ = "symptoms"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    code = Column(String(10), nullable=False)
+    name = Column(String(100), nullable=False)
+
+    # Define the relationship between Diagnosis and Symptom
+    diagnosis = relationship("Diagnosis", backref="symptom")
+
+
+# Define the Diagnosis table
+@generic_repr
+class Diagnosis(Base, Timestamp):
+    __tablename__ = 'diagnoses'
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    business_id = Column(Integer, ForeignKey('businesses.id'), nullable=False)
+    symptom_id = Column(Integer, ForeignKey('symptoms.id'), nullable=False)
+    is_diagnosed = Column(Boolean, nullable=False)
+
+    # Define the relationship between Diagnosis and Business
+    business = relationship("Business", backref="diagnoses")
+
+    # Define the relationship between Diagnosis and Symptom
+    symptom = relationship("Symptom", backref="diagnoses")
